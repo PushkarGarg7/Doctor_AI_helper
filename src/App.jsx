0c33fdd230a9fc4222dd153b62042fea70cca587
AbhinavDev2002 <abhinavagg2002@gmail.com>
2024-10-29 22:00:47 +0530

    FrontEnd Initialise

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './App.css'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+function App() {
+
+  return (
+    <BrowserRouter>
+      <Routes>  
+        <Route path='/' element = {<LandingPage/>}/>
+      </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default App
