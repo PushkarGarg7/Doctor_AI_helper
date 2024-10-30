0baab82f4a0aca89915b779d0ee7acfc5b273495
Shaunak-Gupta-dev <shaunak5424@gmail.com>
2024-10-30 13:06:47 +0530

    changed folder name, and now age and gender taken into consideration

---
+++ Backend/agents.py
@@ -0,0 +1,26 @@
+from crewai import Agent
+from tools import serper_tool
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+import os
+os.environ["OPENAI_API_KEY"] = os.getenv("OPENAI_API_KEY")
+os.environ["OPENAI_MODEL_NAME"]="gpt-4-0125-preview"
+
+symptom_agent = Agent(
+    role="symptom finder",
+    goal="find symptoms for the top 3 diseases: {diseases} based on age {age} and gender {gender}",
+    backstory="This agent specializes in extracting age- and gender-specific symptoms from official medical sources.",
+    verbose=True,
+    tools=[serper_tool],
+    allow_delegation=True
+)
+
+question_agent = Agent(
+    role="question generator",
+    goal="generate questions for a doctor based on symptoms of {diseases} considering age {age} and gender {gender}",
+    backstory="This agent generates diagnostic questions tailored to age and gender, to help doctors confirm diseases.",
+    verbose=True,
+)
